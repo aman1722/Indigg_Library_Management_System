f86c16d3ab8fb8724be86c0b890f54f18d08e816
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-10-06 18:55:59 +0530

    ratelimiter count increased

--- middlewares/rateLimiter.middleware.js
+++ middlewares/rateLimiter.middleware.js
@@ -4,7 +4,7 @@ const rateLimit = require("express-rate-limit");
 // Create a rate limiter instance------>
 const rateLimiter = rateLimit({
   windowMs: 10 * 60 * 1000,
-  max: 20, 
+  max: 50, 
   message: "Too many requests from this IP, please try again after some time.",
 });
 
